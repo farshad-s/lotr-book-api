4391c3caa6ddd21fcd9712f514978df45e872c95
Farshad Shafii <farshad.shafii@gmail.com>
2020-12-22 21:56:23 +0000

    added pics for books

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 fetch("https://the-one-api.dev/v2/book")
   .then((response) => response.json())
-  .then(() => {})
+  .then((data) => {
+    let bookPicArray = [
+      "https://cdn.waterstones.com/bookjackets/large/9780/2611/9780261103573.jpg",
+      "https://cdn.waterstones.com/bookjackets/large/9780/2611/9780261103580.jpg",
+      "https://cdn.waterstones.com/bookjackets/large/9780/2611/9780261103597.jpg",
+    ];
+  })
   .catch((error) => {
     console.log("error is", error);
   });
